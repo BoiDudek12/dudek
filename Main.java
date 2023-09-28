51a709cdb982efc2bd22e8eb1e08a9b9905be979
BoiDudek12 <146319225+BoiDudek12@users.noreply.github.com>
2023-09-28 10:50:47 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dudek");
+
+        int i = 1;
+        System.out.println(i);
+        double f = 2.0;
+        long l = 2L;
+
+        Integer i2 = 2;
+        Double f2 = 2.0;
+        Long l2 = 2L;
+
+        //char w = 'X';
+        //String name = "adam";
+
+       // String kto = "ja ty wy oni co was boli";
+       // System.out.println(kto);
+
+     //   int j = 0;
+       // while(j < 15){
+           // System.out.println(j);
+           // j++;
+       /// }
+      ////  int o = 0;
+       // while(o <  234){
+         //   System.out.println(o++);
+       // }
+        //int t = 0;
+       // while(t < 15){
+      //      System.out.println(name);
+      //      t++;
+      //  }
+      //  for(int r = 0; r < 15; r++) {
+      //      System.out.println(r);
+      //  }
+
+        do {if (i==10)
+            System.out.println("\nHello");
+           System.out. print(String.valueOf(i) + " ");
+            i++;}
+        while (i<=10);
+
+        a();
+        b();
+        c();
+
+
+    }
+
+    private static void a() {
+        System.out.println("funkcja a");
+    }
+    private static void b() {
+        System.out.println("funkcja b");
+    }
+    private static void c() {
+        System.out.println("funkcja c");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
